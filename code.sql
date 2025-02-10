a0400c994ce4048092a39e6573e6746a90870225
Sr1jan27 <singhsrijan809@gmail.com>
2025-02-11 01:13:15 +0530

    sql

--- code.sql
+++ code.sql
@@ -25,3 +25,9 @@ MovingAverage AS (
 SELECT visited_on, avg_time_spent
 FROM MovingAverage
 ORDER BY visited_on;
+
+
+
+-- There is a database that contains a website's traffic data over a period of 30 days. The first table contains users' information including the user type (user, crawler, admin). The second table consists of the time of each visit to the website, the id of the visitor, time spent on the website in seconds. Write an SQL query to show the 3 day moving average of time spent on the website for users.user_type = 'user'.
+
+-- Note: avg time spent must have 4 decimal digits and rounded off (example: 5/3 = 1.6667, 5/2 = 2.5000 and 1/3 = 0.3333)
